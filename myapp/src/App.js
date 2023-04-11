7b5ace8217c146c50d1b0015156fac4a8f9931c0
PIYUSH GHOSH <impiyushghosh@gmail.com>
2023-04-12 01:44:12 +0530

    added the code snippets and all routes . Also added the home section layout along with the Exchange section of the app

---
+++ myapp/src/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Header from "./Components/Header"
+import Home from "./Components/Home"
+import Coins from "./Components/Coins"
+import CoinDetails from "./Components/CoinDetails"
+import Exchanges from "./Components/Exchanges"
+function App() {
+  return (
+    <Router>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coin/:id" element={<CoinDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
